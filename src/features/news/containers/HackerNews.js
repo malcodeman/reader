66e45a655c7b8a9055b45d212ab963ab0018adb9
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2020-01-11 17:42:49 +0100

    Refactors hacker news component

--- src/features/news/containers/HackerNews.js
+++ src/features/news/containers/HackerNews.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import Header from "../components/Header";
 import Loading from "../components/Loading";
@@ -7,58 +7,39 @@ import Post from "../components/Post";
 import { List, ListItem } from "./Home";
 import { changeSource, requestBestStories } from "../actions/actions_news";
 
-class HackerNews extends React.Component {
-  componentDidMount = () => {
-    const { changeSource, requestBestStories } = this.props;
+function HackerNews(props) {
+  const source = useSelector(state => state.news.source);
+  const loading = useSelector(state => state.news.loading);
+  const posts = useSelector(state => state.news.posts);
+  const dispatch = useDispatch();
 
-    changeSource("hacker news");
-    requestBestStories();
-  };
-  render() {
-    const { source, loading, posts } = this.props;
+  React.useEffect(() => {
+    dispatch(changeSource("hacker news"));
+    dispatch(requestBestStories());
+  }, [dispatch]);
 
-    return (
-      <>
-        <Header title={source} />
-        {loading ? (
-          <Loading>Loading...</Loading>
-        ) : (
-          <List>
-            {posts.map(post => (
-              <ListItem key={post.id}>
-                <Post
-                  url={post.url}
-                  title={post.title}
-                  upvotes={post.upvotes}
-                  author={post.author}
-                  comments={post.comments}
-                  created_at={post.created_at}
-                />
-              </ListItem>
-            ))}
-          </List>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <Header title={source} />
+      {loading && <Loading>Loading...</Loading>}
+      {!loading && posts && (
+        <List>
+          {posts.map(post => (
+            <ListItem key={post.id}>
+              <Post
+                url={post.url}
+                title={post.title}
+                upvotes={post.upvotes}
+                author={post.author}
+                comments={post.comments}
+                created_at={post.created_at}
+              />
+            </ListItem>
+          ))}
+        </List>
+      )}
+    </>
+  );
 }
 
-const mapStateToProps = state => {
-  return {
-    source: state.news.source,
-    loading: state.news.loading,
-    posts: state.news.posts
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    changeSource: source => dispatch(changeSource(source)),
-    requestBestStories: () => dispatch(requestBestStories())
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(HackerNews);
+export default HackerNews;
